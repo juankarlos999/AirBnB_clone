13d79fc0022161a119f5600eba9bfcccfe47779b
juankarlos999 <juan_karlos_999@hotmail.com>
2020-06-26 20:11:26 -0500

    [FIX]: style, edit fuctions __str__, to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -16,25 +16,31 @@ class BaseModel:
         """
         Instantiation method or constructor with *args and **kwargs
         """
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         self.id = str(uuid4())
 
     def save(self):
         """
-        updates the public instance updated_at attribute with the current datetime
+        updates the public instance updated_at attribute
+        with the current datetime
         """
         self.updated_at = datetime.now().isoformat()
         return self.updated_at
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all keys/values of __dict__
+        of the instance
         """
-        self.__dict__["__class__"] = self.__class__.__name__
-        return self.__dict__
+        var_dict = self.__dict__.copy()
+        var_dict["__class__"] = self.__class__.__name__
+        var_dict["created_at"] = self.created_at.isoformat()
+        var_dict["updated_at"] = self.updated_at.isoformat()
+        return var_dict
 
     def __str__(self):
         """print: [<class name>] (<self.id>) <self.__dict__>
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        print_ = "[{}] ({}) {}"
+        return print_.format(self.__class__.__name__, self.id, self.__dict__)
