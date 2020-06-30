1b18f73dc71c6ed9b0350abf0999cea38ade9d37
Leidysalda <leidysalda1@gmail.com>
2020-06-30 01:29:11 +0000

    Add file console.py

--- console.py
+++ console.py
@@ -11,5 +11,29 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb)'
 
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """exit interpreter Crtl + D"""
+        print()
+        return True
+
+    def emptyline(self):
+        """
+        Called when an empty line is entered in response to the prompt. If this
+        method is not overridden, it repeats the last nonempty comman entered.
+        cmd execute precmd, onecmd and postcmd methods sequentially. onecmd is
+        the main one which exetues the given line.
+        This method check the line, if line is empty it calls the emptyline
+        method and it returns the last command which is a global variable called
+        as 'lastcmd'. The method emptyline(), override the variable 'lastcmd'
+        asigning double quotes.
+        """
+        if self.lastcmd:
+            self.lastcmd = ""
+            return self.onecmd('\n')
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
