c76aa52c0738f11f730f8debb0cf6309d997aafb
Leidysalda <leidysalda1@gmail.com>
2020-07-01 05:37:17 +0000

    Add file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """Test FileStorage"""
-
 import unittest
 from models.base_model import BaseModel
 from models import storage
-import pep8
 
 
+class test_FileStorage(unittest.TestCase):
+    """Test file storage"""
+
+    def test_all(self):
+        """test all"""
+        model = BaseModel()
+        model.save()
+        new_object = storage.all()
+        self.assertEqual(dict, type(new_object))
+
+if __name__ == '__main__':
+    unittest.main()
