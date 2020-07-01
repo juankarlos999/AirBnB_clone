dc77d5cabe7a1bd7427bcf22f68c069990cb171d
juankarlos999 <juan_karlos_999@hotmail.com>
2020-07-01 02:56:10 -0500

    [ADD]: module User

--- console.py
+++ console.py
@@ -9,6 +9,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.base_model import BaseModel
+from models.user import User
 from models import storage
 
 
